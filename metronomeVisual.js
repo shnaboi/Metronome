d944d53182e0ec073480fa0ac430f5d5eea0d080
shnaboi <shane.karner@gmail.com>
2022-12-10 19:54:22 -0500

    Canvas black background via CSS. Add animation to canvas

--- metronomeVisual.js
+++ metronomeVisual.js
@@ -3,21 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let canvas = document.getElementById('canvas'); 
     let c = canvas.getContext('2d');
 
-    canvas.canvasHeight = 130;
-    canvas.canvasWidth = 420;
+    canvas.height = 130;
+    canvas.width = 420;
 
     c.fillRect(0, 0, 420, 130);
 
+    let x = 1;
 
-    c.beginPath();
-    c.strokeStyle = 'white';
-    c.moveTo(10, 0);
-    c.lineTo(10, 130);
-    c.stroke();
+    function animate() {
+        requestAnimationFrame(animate);
+        x += 1;
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        c.beginPath();
+        c.strokeStyle = 'white';
+        c.moveTo(x, 0);
+        c.lineTo(x, 130);
+        c.stroke();
+
+}
+
+    animate();
 
 
-
-
-
-    
 })
