e93989467520b7f75bab6ccf34743722f78ac6f3
shnaboi <shane.karner@gmail.com>
2022-12-12 13:25:55 -0500

    Add functionality to play button to start animation.

--- metronomeVisual.js
+++ metronomeVisual.js
@@ -9,8 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
     c.fillRect(0, 0, 420, 130);
 
     let x = 1;
-    let dx = 5;
+    let dx = 2.3;
     let bpm = 120;
+    let visual = false;
+
+    const playButton = document.getElementById('play');
+    const pauseButton = document.getElementById('pause');
+
+    if (visual == false) {
+        playButton.addEventListener('click', (event) => {
+            animate();
+            visual = true;
+            console.log(visual);
+        })
+    } else if (visual == true) {
+
+    }
+
+    pauseButton.addEventListener('click', (event) => {
+        visual = false;
+        console.log(visual);
+    })
+
+    if (visual === true) {
+        animate();
+    } 
 
     function animate() {
         requestAnimationFrame(animate);
@@ -21,18 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
         c.moveTo(x, 0);
         c.lineTo(x, 130);
         c.stroke();
-        if (x >= canvas.width) {
+        if (x >= canvas.width || x <= 0) {
             dx = -dx;
         }
+    }
 
-        if (x <= 0) {
-            dx = -dx;
-        }
-
-
-}
-
-    animate();
 
 
 })
